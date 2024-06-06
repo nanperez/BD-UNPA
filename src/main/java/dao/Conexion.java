c27e58f77aa5cbc7c355339b4b30f44361007bd5
naperez <naperez@nancy-pc>
2024-06-06 12:36:48 -0600

    se agrego implementacion buscar por nombre

--- src/main/java/dao/Conexion.java
+++ src/main/java/dao/Conexion.java
@@ -13,11 +13,12 @@ import java.sql.SQLException;
  * @author naperez
  */
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/Libreria";
+    private static final String URL = "jdbc:mysql://localhost:3306/Libreria2";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
+        System.out.println("CONEXION REALIZADA");
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
